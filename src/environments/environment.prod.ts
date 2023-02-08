1e06dda164c46ac9060ea60eac39296ded91107d
Erfan Hemayati <ehemayati@myseneca.ca>
2023-02-08 16:58:58 -0500

    changing the api URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   clientID: "7b540157af564793a75fe48b3df06c48",
   clientSecret: "f9164afa66484d8bb87962f37a5564af",
-  userAPIBase:"http://localhost:8080"  
- // userAPIBase:"https://radiant-taiga-89817.herokuapp.com"
+  //userAPIBase:"http://localhost:8080"  
+ userAPIBase:"https://unusual-clam-gilet.cyclic.app"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,8 +6,8 @@ export const environment = {
   production: false,
   clientID: "7b540157af564793a75fe48b3df06c48",
   clientSecret: "f9164afa66484d8bb87962f37a5564af",
-  userAPIBase:"http://localhost:8080"
-  //userAPIBase:"https://radiant-taiga-89817.herokuapp.com"
+  //userAPIBase:"http://localhost:8080"
+  userAPIBase:"https://unusual-clam-gilet.cyclic.app"
 };
 
 /*
