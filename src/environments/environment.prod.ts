21dda44b04ccfb6fec17b8754a250ec61d86940b
Erfan Hemayati <ehemayati@myseneca.ca>
2023-02-10 01:26:08 -0500

    testing

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   clientID: "7b540157af564793a75fe48b3df06c48",
   clientSecret: "f9164afa66484d8bb87962f37a5564af",
   //userAPIBase:"http://localhost:8080"  
- userAPIBase:"https://unusual-clam-gilet.cyclic.app"
+ userAPIBase:"https://unusual-clam-gilet.cyclic.app"s
 };
