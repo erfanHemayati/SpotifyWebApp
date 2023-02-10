23df6c260066813f3216ef4d703713ab437fadf0
Erfan Hemayati <ehemayati@myseneca.ca>
2023-02-09 19:41:38 -0500

    changing env variables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,11 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  clientID: "7b540157af564793a75fe48b3df06c48",
-  clientSecret: "f9164afa66484d8bb87962f37a5564af",
+//  production: false,
   //userAPIBase:"http://localhost:8080"
-userAPIBase:"https://unusual-clam-gilet.cyclic.app"
 };
 
 /*
