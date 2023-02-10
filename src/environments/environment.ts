cc53f768d52d2383315a12b075aea0c3c51d29d2
Erfan Hemayati <ehemayati@myseneca.ca>
2023-02-09 20:15:50 -0500

    changing

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-//  production: false,
+  production: false,
+  clientID: "7b540157af564793a75fe48b3df06c48",
+  clientSecret: "f9164afa66484d8bb87962f37a5564af",
   //userAPIBase:"http://localhost:8080"
+userAPIBase:"https://unusual-clam-gilet.cyclic.app"
 };
 
 /*
